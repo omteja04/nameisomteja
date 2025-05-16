522c2713bb38f28261f08519127f064470705e0e
omteja04 <22P31A0539@acet.ac.in>
2025-05-17 04:41:55 +0530

    Added Experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -86,7 +86,6 @@ const Experience = () => {
                                         );
                                     })}
                                 </div>
-
                                 {/* Content */}
                                 <motion.div
                                     key={activeTab}
